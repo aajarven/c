f68c85507b87ccc89966b41b4dfae7f6421883d4
aajarven <anni.jarvenpaa@gmail.com>
2015-09-18 19:26:08 +0300

    ekaviikko

--- Week1/separateDigits/test/test_source.c
+++ Week1/separateDigits/test/test_source.c
@@ -17,9 +17,16 @@ START_TEST(test_digits) {
     char userString[1000];
     char message[100] = "";
     char* rightString = "9 0 8 7 6 5 \n";
+    char* rightv2String = "9 0 8 7 6 5\n";
     fgets(userString, 1000, fp);
+    if (strlen(userString)==strlen(rightv2String)) {
+       //Testing without the last space
+       int ret = mycompare(userString, rightv2String, message);
+       ck_assert_msg(!ret, "Your output:\n%s\nRight output:\n%s\n%s\n", userString, rightString, message);
+    }
+
     int ret = mycompare(userString, rightString, message);
-    ck_assert_msg(!ret, "Your output:\n%s\nRight output:\n%s\n%s\n", userString, rightString, message);
+    ck_assert_msg(!ret, "Your output:\n%s\nRight output:\n%s\n%s\n. Hint: Try only one space after every number\n", userString, rightString, message);
     fclose(fp);
 }
 END_TEST
