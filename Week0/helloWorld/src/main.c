865c548e2c0d6815cef5157da40e371db33ab787
aajarven <anni.jarvenpaa@gmail.com>
2015-10-21 21:21:42 +0300

    implicit declaration -testailua

--- Week0/helloWorld/src/main.c
+++ Week0/helloWorld/src/main.c
@@ -4,7 +4,13 @@
 /* Objective: Test that your development environment works and you can send tasks to the tmc-server.
  * Make sure that this task prints "Hello World!" and submit task to the server.*/
 
+
 int main() {
-    hello_world();
+    float arvo = testi();
+    printf("%f\n", arvo);
     return 0;
+}
+
+float testi(){
+    return 1.5;
 }
